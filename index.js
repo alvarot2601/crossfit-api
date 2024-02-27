f38eabeab8904052f5651fad5ec8e4dc1904a93f
unknown <alvarot2601@gmail.com>
2024-02-27 09:50:09 +0100

    refactorizo workoutRoutes.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // In src/index.js
 const express = require("express");
 // *** ADD ***
-const v1Router = require("./src/v1/routes/");
+const v1WorkoutRouter = require("./src/v1/routes/workoutRoutes.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 // *** ADD ***
-app.use("/api/v1", v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
