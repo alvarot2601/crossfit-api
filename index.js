f8a536a78be6cfb9ee40911d2a4b41cd3a03190f
unknown <alvarot2601@gmail.com>
2024-02-27 12:22:25 +0100

    añado logica a archivo  database/workout.js

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require("express");
 const v1WorkoutRouter = require("./src/v1/routes/workoutRoutes.js");
 
 const app = express();
+//const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
 
 // *** ADD ***
+//app.use(bodyParser.json());
+app.use(express.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
