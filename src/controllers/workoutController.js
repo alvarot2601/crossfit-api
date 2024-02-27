6a738115ac2852563ae088c89d58ef3a46071d08
unknown <alvarot2601@gmail.com>
2024-02-27 10:03:56 +0100

    Añado metodos de workoutServices.js en  workoutController

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,21 +1,27 @@
+const workoutServices = require("../services/workoutServices.js");
 // In src/controllers/workoutController.js
 const getAllWorkouts = (req, res) => {
+    const allWorkouts = workoutServices.getAllWorkouts();
     res.send("Get all workouts");
   };
   
   const getOneWorkout = (req, res) => {
+    const allWorkouts = workoutServices.getAllWorkouts();
     res.send("Get an existing workout");
   };
   
   const createNewWorkout = (req, res) => {
+    const createdWorkout = workoutServices.createNewWorkout();
     res.send("Create a new workout");
   };
   
   const updateOneWorkout = (req, res) => {
+    const updatedWorkout = workoutServices.updateOneWorkout();
     res.send("Update an existing workout");
   };
   
   const deleteOneWorkout = (req, res) => {
+    const deletedWorkout = workoutServices.deleteOneWorkout();
     res.send("Delete an existing workout");
   };
   
