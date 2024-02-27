e8e88776c804b6d7c0a251a80dba4476e1a70a78
unknown <alvarot2601@gmail.com>
2024-02-27 10:41:54 +0100

    Valido el body obtenido en metodo createNewWorkout

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -2,35 +2,52 @@ const workoutServices = require("../services/workoutServices.js");
 // In src/controllers/workoutController.js
 const getAllWorkouts = (req, res) => {
     const allWorkouts = workoutServices.getAllWorkouts();
-    res.send({status: 'OK', data: allWorkouts});
-  };
-  
-  const getOneWorkout = (req, res) => {
+    res.send({ status: 'OK', data: allWorkouts });
+};
+
+const getOneWorkout = (req, res) => {
     const allWorkouts = workoutServices.getAllWorkouts();
     res.send("Get an existing workout");
-  };
-  
-  const createNewWorkout = (req, res) => {
-    const createdWorkout = workoutServices.createNewWorkout();
-    res.send("Create a new workout");
-  };
-  
-  const updateOneWorkout = (req, res) => {
+};
+
+const createNewWorkout = (req, res) => {
+    const { body } = req;
+    if (
+        !body.name ||
+        !body.mode ||
+        !body.equipment ||
+        !body.exercises ||
+        !body.trainerTips
+    ) {
+        return;
+    }
+    const newWorkout = {
+        name: body.name,
+        mode: body.mode,
+        equipment: body.equipment,
+        exercises: body.exercises,
+        trainerTips: body.trainerTips,
+      };
+    const createdWorkout = workoutServices.createNewWorkout(newWorkout);
+    res.status(201).send({status: 'OK', data: createdWorkout});
+};
+
+const updateOneWorkout = (req, res) => {
     const updatedWorkout = workoutServices.updateOneWorkout();
     res.send("Update an existing workout");
-  };
-  
-  const deleteOneWorkout = (req, res) => {
+};
+
+const deleteOneWorkout = (req, res) => {
     const deletedWorkout = workoutServices.deleteOneWorkout();
     res.send("Delete an existing workout");
-  };
-  
-  module.exports = {
+};
+
+module.exports = {
     getAllWorkouts,
     getOneWorkout,
     createNewWorkout,
     updateOneWorkout,
     deleteOneWorkout,
-  };
+};
 
-  925288484
+925288484
